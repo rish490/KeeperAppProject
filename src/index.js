79ec18aaec4f070812ebfe1ec3f7dcd42f1e3eed
rish490 <88023315+rish490@users.noreply.github.com>
2021-10-09 13:56:12 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+//styled and completed version of keeper app
